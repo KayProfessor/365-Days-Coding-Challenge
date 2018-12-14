5600876358f75a59b4fa490db05940768f048a14
KayProfessor <kayprofessor@gmail.com>
2018-12-13 20:28:19 -0500

    Day-52 of Coding day December 13, 2018 #TheCodingWay #365CodingPhaseChallenge

---
+++ Day-52 Javascript-start/index.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Tweet your messages below");
+var tweetCount = tweet.length;
+alert("You have written " + tweetCount + " characters, you have " + (140  -tweetCount) + " characters remaining.");
